5189b36aac765e333210ce117ca670d4f7c5afbb
Alyaa86 <alateya86@hotmail.com>
2018-04-30 21:13:41 +0300

    30 April 2018 - home page done

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View,  } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, TextInput } from 'react-native';
 import { ScrollView, Image } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body, Right, Title , List, Tab, Tabs} from 'native-base';
+import { Footer, FooterTab, Form, Item, Input, Label } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { NativeRouter, Route, Link, Switch } from 'react-router-native'
 import Q1 from './Q1.js';
@@ -10,7 +11,8 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
         this.satate= {
-            img : ''
+            img : '',
+            text: 'Useless Placeholder'
         }
     }
   render() {
@@ -18,22 +20,42 @@ export default class App extends React.Component {
       <ImageBackground  source={require('../images/bkgrnd.gif')} 
           style={{height: 700, width: null, flex: 9}}>
       <Container style={styles.container}>
+
         <Header>
           <Left/>
           <Body>
-            <Title>UNDERTONE</Title>
+            <Text>UNDERTONE APP</Text>
           </Body>
           <Right />
         </Header>
-             <CardItem>
-              <Right>
-                <Button FULL transparent textStyle={{color: '#87838B'}}>
-                  <Text>START YOUR TEST NOW</Text>
-                </Button>
-              </Right>
-            </CardItem>
-          </Card>
-        </Content>
+        <Content>
+        <Text style={styles.bigWhite}> READY TO KNOW YOUR UNDERTONE?</Text>
+        
+          <Form style={styles.form}>
+            <Item stackedLabel >
+              <Label>Write Your Name Here .. </Label>
+              <Input style={styles.WhiteFont} />
+            </Item>
+             <Item  stackedLabel>
+              <Label>Give us Your Email to send you the results </Label>
+              <Input style={styles.WhiteFont} />
+            </Item>
+          </Form>
+ 
+        <Button rounded info style={styles.button}>
+            <Text style={styles.WhiteFont}>GO</Text>
+          </Button>
+    
+         </Content>  
+        
+        
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Text> CopyRights to Reem and Alyaa </Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
       </ImageBackground>
     );
@@ -49,6 +71,39 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
     },
   Content: {
-    top: 30
+    top: 75,
+    marginLeft: 0,
+    alignSelf: "center"
+  },
+  bigWhite: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 50,
+    alignSelf: "center"
+  },
+  form:{
+    top: 50,
+    alignSelf: "center"
+  },
+  WhiteFont: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    
+    
+    
+  },
+  button:{
+    flex: 1 ,
+    position: 'absolute',
+    bottom:-170,
+    left:250, 
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+    
+    
+
+
   }
 });
